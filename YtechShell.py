28ec382d2b195afe225ab03b8651b960e8b06e13
Robinson Paspuel <robin_pas@hotmail.com>
2021-05-29 11:29:58 -0500

    Lexer contenct

--- YtechShell.py
+++ YtechShell.py
@@ -1,3 +1,7 @@
+import YTechCode_lexer
+
 while True:
     text = input('YtechCode > ')
-    print(text)
+    result, error = lexer.run(text)
+    if error: print (error.error_string())
+    else: print(result)
